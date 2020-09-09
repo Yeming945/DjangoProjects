e42d466ab26e027b32ef63d29e3e153e90b99005
Flaming <yeming945@qq.com>
2020-09-09 11:12:31 +0800

    1.3.2 查看文章内容

--- Mysite/blog/views.py
+++ Mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import BlogArticles
 
@@ -6,3 +6,9 @@ from .models import BlogArticles
 def blog_title(request):
     blogs = BlogArticles.objects.all()
     return render(request, 'blog/titles.html', {'blogs': blogs})
+
+
+def blog_article(request, article_id):
+    article = get_object_or_404(BlogArticles, id=article_id) # 避免访问出现不存在的ID值报错
+    pub = article.publish
+    return render(request, "blog/content.html", {"article": article, 'publish': pub})
