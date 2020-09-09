c2f68eaa96711c36f2ceab661177075b8489b088
Flaming <yeming945@qq.com>
2020-09-09 09:44:49 +0800

    1.2.2 发布博客文章

--- Mysite/blog/admin.py
+++ Mysite/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogArticles
+
+
+class BlogArticlesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish')  # 控制显示字段
+    list_filter = ('publish', 'author')  # 过滤字段
+    search_fields = ('title', 'body')  # 搜索字段
+    raw_id_fields = ('author', )
+    date_hierarchy = 'publish'  # 日期归档
+    ordering = ['-publish', 'author']  # 排序
+
+
+admin.site.register(BlogArticles, BlogArticlesAdmin)
